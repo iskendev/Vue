4be0eddf064acb6676a501799f80a086ac727a6b
Chinaliev Iskender <isken001@gmail.com>
2020-06-21 21:26:25 +0600

    routes & components added

--- intelli-crm/src/router.js
+++ intelli-crm/src/router.js
@@ -8,9 +8,25 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Login.vue')
+    },
+    {
       // path: '/',
-      // name: 'home',
-      // component: Home
+      // name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
     {
       // path: '/about',
