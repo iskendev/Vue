944aa68d322e95e5520f61dfb9018fa9fb7832f6
Chinaliev Iskender <isken001@gmail.com>
2020-06-27 23:16:03 +0600

    some files added

---
+++ intelli-crm/src/components/RelaxerKeyboards.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+  div
+    h1 Home
+</template>
+
+<script>
+export default {
+  name: '',
+  methods: {},
+  computed: {},
+  mounted: {},
+  watch:{}
+}
+</script>
+
+<style lang="scss">
+
+</style>
