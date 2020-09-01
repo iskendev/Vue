426e179987c2a586d32a5d8e61ad57ff5c9f1776
iskender <ichinaliev@intelliants.com>
2020-09-01 20:18:02 +0600

    dispalying info properly

--- intelli-crm/src/views/Clockify.vue
+++ intelli-crm/src/views/Clockify.vue
@@ -33,7 +33,7 @@
                     span.description {{ entry.description }}:
                     span.client-name {{ entry.clientName }}
                   div.entry-info__details
-                    div.entry-tags(v-if='entry.tagIds === null' title="No tags")
+                    div.entry-tags(v-if='entry.tagIds === null || !entry.tagIds.length' title="No tags")
                       i.fas.fa-tag
                     div.entry-tags(v-else)
                       span.tags(v-for='tag in entry.tagTitles') {{ tag | cutExtraWords(entry.tagTitles) }}
@@ -87,10 +87,39 @@ export default {
       }
     }
 
+    // Setting a project name
     const setProjectName = (name) => {
       projectTitle.value = name
     }
 
+    const filterProjectsByEntriesLength = (projects) => {
+      let projectsLengthArr = [];
+      projects.forEach(project => {
+        if (project.dates.length) {
+          projectsLengthArr.push(project.dates.length)
+        }
+      })
+      return projectsLengthArr
+    }
+
+    const setProjectWithMaxEntries = (projects, projectsLengthArr) => {
+        let max = Math.max.apply(null, projectsLengthArr)
+        projects.filter(project => {
+          if (project.dates.length == max)
+            projectTitle.value = project.name
+        })
+    }
+
+    const projectNameHandlers = () => {
+      if (clockifyData.value) {
+        filterProjectsByEntriesLength(clockifyData.value.projects)
+
+        const projectsLengthArr = filterProjectsByEntriesLength(clockifyData.value.projects)
+        setProjectWithMaxEntries(clockifyData.value.projects, projectsLengthArr)
+      }
+    }
+
+    // watchers
     watch(userInfo, async () => {
       if (userInfo.value.clockifyKey) {
         await $store.dispatch('getUserData', userInfo.value.clockifyKey)
@@ -102,51 +131,29 @@ export default {
 
     watch(clockifyData, () => {
       loading.value = false
-
-      // TODO separate function
-      if (clockifyData.value) {
-        console.log('sdcsdc');
-        let projectsLengthArr = [];
-        clockifyData.value.projects.forEach(project => {
-          if (project.dates.length) {
-            projectsLengthArr.push(project.dates.length)
-          }
-        })
-        let max = Math.max.apply(null, projectsLengthArr)
-        clockifyData.value.projects.filter(project => {
-          if (project.dates.length == max)
-            projectTitle.value = project.name
-            console.log("setup -> projectTitle.value", projectTitle.value)
-        })
-      }
-
+      projectNameHandlers()
       console.log(clockifyData.value);
     })
 
     onMounted(async () => {
-      if (userInfo.value.clockifyKey)
+      if (userInfo.value.clockifyKey) {
         loading.value = false
-
-
-      // TODO separate function
-      if (clockifyData.value) {
-        console.log('sdcsdc');
-        let projectsLengthArr = [];
-        clockifyData.value.projects.forEach(project => {
-          if (project.dates.length) {
-            projectsLengthArr.push(project.dates.length)
-          }
-        })
-        let max = Math.max.apply(null, projectsLengthArr)
-        clockifyData.value.projects.filter(project => {
-          if (project.dates.length == max)
-            projectTitle.value = project.name
-            console.log("setup -> projectTitle.value", projectTitle.value)
-        })
       }
+      projectNameHandlers()
     })
 
-    return { clockifyKey, clockifyData, userInfo, assignUserClockifyKey, loading, projectTitle, setProjectName, clockifyDataProjects }
+    return {
+      clockifyKey,
+      clockifyData,
+      userInfo,
+      assignUserClockifyKey,
+      loading,
+      projectTitle,
+      setProjectName,
+      clockifyDataProjects,
+      filterProjectsByEntriesLength,
+      setProjectWithMaxEntries
+    }
   }
 }
 </script>
